c092efd8998f21a31e0a38d196831c4387723be9
usuarioingeniero123 <106457167+usuarioingeniero123@users.noreply.github.com>
2022-05-28 20:32:11 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+#Ejercicioretador1.
+superficiedelestadodesinaloakilometros=57,365
+precipitacionanualpromedioestadodesinaloa=790.1
+variablepoblacionmujeres=1532128
+variablepoblacionhombre=1494815
+poblacióntotal=variablepoblacionmujeres+variablepoblacionhombre
+print("La poblacion total es ", poblacióntotal,"HABITANTES" )
+
+porcentajeculiacan= 33.57
+porcentajemazatlan=16.57
+
+totalporcentajedeculiacanymazatlan=porcentajeculiacan+porcentajemazatlan
+print("El porcentaje total es: ", totalporcentajedeculiacanymazatlan,"%")
+temperaturaanual=("La temperatura media anual es de 25.45")
+tiposdeclima=("Los tipo de clima son: Cálido,subhumedo, seco y semiseco")
+print(temperaturaanual)
+print(tiposdeclima)
